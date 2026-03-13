ab75d07996f97f396482c61bbdfb3a3fffe40e47
Primal CI <developer@primal.net>
2026-03-13 19:09:05 +0000

    Update PrimalShared to 0.1.15

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PrimalShared",
-            url: "https://github.com/PrimalHQ/primal-shared-ios/releases/download/0.1.14/PrimalShared.xcframework.zip",
-            checksum: "3059b4982b847b5be4ba7f42ca17ac42e98ca67ddcd3fccfffeb6020e0f9ffdc"
+            url: "https://github.com/PrimalHQ/primal-shared-ios/releases/download/0.1.15/PrimalShared.xcframework.zip",
+            checksum: "57266b534db8170e85aaced716ffe7ab6a49e737890055d366363b5224191cf1"
         )
     ]
 )
